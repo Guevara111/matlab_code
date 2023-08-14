a1e5f4e0f074cd65ae8e79c685a751517dee2bb3
Guevara <55071222+Guevara111@users.noreply.github.com>
2023-08-14 13:59:49 +0800

    Add files via upload

---
+++ 图像修复/main1.m
@@ -0,0 +1,39 @@
+% 处理图片1
+close all;clear;
+
+addpath(genpath('FFM_inpaint'));
+addpath(genpath('get_mask'));
+
+I1 = imread('img1.JPG');
+I1 = imresize(I1, 0.4); %为了提高运算速度，缩小图片
+I1_gray = rgb2gray(I1);
+
+line1_mask = get_mask_1(I1_gray, floor(509*size(I1,1)/719));
+
+figure;imshow(line1_mask);
+new_gray=I1_gray;
+new_gray(line1_mask>0)=255;
+%imshow(new_gray);
+
+result_img = inpaint_FMM( I1, uint8(line1_mask/255), 3 );
+figure;imshow(result_img);
+
+%%
+close all;clear;
+addpath(genpath('FFM_inpaint'));
+addpath(genpath('get_mask'));
+
+I1 = imread('a.jpg');
+%%
+% I2=imadjust(I1,[.2 .3 0; .6 .7 1],1.5);
+
+
+I=rgb2gray(I1*0.8);
+BW = imbinarize(I,'adaptive');
+line1_mask=imcomplement(BW);
+
+result_img = inpaint_FMM( I1, uint8(line1_mask), 12);
+
+figure()
+imshow(result_img)
+% imshow(result_img)
